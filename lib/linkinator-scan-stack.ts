ba4640ae3eeda2856cdbf99b3df0cc0203a6362a
Eleni Papanicolas <elenipapanicolas@gmail.com>
2024-02-14 17:35:33 -0500

    return to use lambda > nodejslambda

--- lib/linkinator-scan-stack.ts
+++ lib/linkinator-scan-stack.ts
@@ -1,6 +1,5 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
 // import * as events from 'aws-cdk-lib/aws-events';
 // import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
@@ -9,8 +8,8 @@ export class LinkinatorScanStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaFn = new lambdaNodeJs.NodejsFunction(this, 'Singleton', {
-      entry: 'lambda/index.ts',
+    const lambdaFn = new lambda.Function(this, 'Singleton', {
+      code: lambda.Code.fromAsset('lambda'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_18_X,
       timeout: Duration.seconds(300),
