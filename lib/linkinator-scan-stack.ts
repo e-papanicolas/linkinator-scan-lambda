e37e85faf09be9045add170b10f02689fbd39271
Eleni Papanicolas <elenipapanicolas@gmail.com>
2024-02-17 16:49:56 -0500

    create eventbridge rule to schedule lambda monthly

--- lib/linkinator-scan-stack.ts
+++ lib/linkinator-scan-stack.ts
@@ -1,8 +1,8 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-// import * as events from 'aws-cdk-lib/aws-events';
-// import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
 import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
@@ -29,12 +29,18 @@ export class LinkinatorScanStack extends Stack {
         resources: ['*'],
       }),
     );
-    
-    // const cronJobRule = new events.Rule(this, 'Rule', {
-    //   schedule: events.Schedule.expression('cron(0 0 1 * *'),
-    // });
 
-    // cronJobRule.addTarget(new targets.LambdaFunction(lambdaFn));
+    const scheduleLambda = new events.Rule(this, "monthlyOnTheFirstDayRule", {
+      schedule: events.Schedule.cron({day: '1', hour: '0', minute: '0'}),
+    });
+
+    scheduleLambda.addTarget(
+      new targets.LambdaFunction(linkinatorFn, {
+        event: events.RuleTargetInput.fromObject({}),
+      })
+    );
+
+    targets.addLambdaPermission(scheduleLambda, linkinatorFn);
     
   }
 }
